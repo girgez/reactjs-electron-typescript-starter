c26760a4d14987cd8b8907bf3ed22478305a3ba2
Công Lý Hoàng <congly1311@gmail.com>
2020-01-08 11:48:36 +0700

    add storybook
    add styled component
    
    add example

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import './App.css';
+import Section from './components/Section';
 import logo from './logo.svg';
 
+const handleClick  = () => {
+  alert("clicked");
+};
+
 const App: React.FC = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //     <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <Section 
+        title="What is Lorem Ipsum?" 
+        titleColor="#123456"
+        action={handleClick}
+        >Lorem Ipsum is simply dummy text of the printing and typesetting industry</Section>
   );
 };
 
---
+++ src/components/Section/styled.tsx
@@ -0,0 +1,29 @@
+// @ts-ignore
+import styled from "styled-components";
+
+type Props = {
+  titleColor: string;
+};
+
+export const SectionTitle = styled.h1`
+  font-size: 20px;
+  color: ${ (props: Props) => props.titleColor ? props.titleColor : 'red' };
+`;
+
+export const SectionContent = styled.p`
+  font-size: 14px;
+  color: black;
+`;
+
+export const SectionAction = styled.button`
+  border: none;
+  background: blue;
+  border-radius: 5px;
+  width: 80px;
+  height: 30px;
+  cursor: pointer;
+  color: white;
+  &:hover {
+    background: #0583FF;
+  }
+`;
